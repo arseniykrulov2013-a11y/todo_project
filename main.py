c028d459107c39fa5a8f586c11a656551bf871df
Arseniy <natali.astafjewa2017@yandex.ru>
2026-04-21 18:35:59 +0300

    Idk

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, session, redirect, render_template, request
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+todo_data =[]
+
+
+@app.route("/")
+def index():
+    if len(todo_data) > 0:
+        return render_template("index.html", data=todo_data)
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1
+    else:
+        session['visits'] = 1
+    return render_template("index.html")
+
+@app.route("/add_item", methods=["POST", "GET"])
+def add_item():
+    text = request.form.get("text")
+    if text:
+        todo_dict = {
+            "name": text,
+            "id": index
+        }
+        todo_data.append(todo_dict)
+        print(todo_dict)
+        return redirect("/")
+    else:
+        return redirect("/")
+
+@app.route("/didit/<id>")
+def didit(id):
+    todo_data.remove(todo_data[int(id)])
+    return redirect("/")
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return redirect("/")
+
+if __name__ == '__main__':
+    app.run()
